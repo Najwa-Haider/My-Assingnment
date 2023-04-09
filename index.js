c3fc5a7c58d4e4d13002c096f76c0dfb1ec19921
Rixon <Rixon@Rixons-MacBook-Pro.local>
2023-04-09 03:26:46 -0700

    add a title

---
+++ index.js
@@ -0,0 +1,23 @@
+//JavaScript Fundamentals - Part 1
+//LECTUR: Values and Variables
+
+/*let country = "Afghanistan";
+var continent = "Asia";
+var population = 7;
+console.log(country);
+console.log(continent);
+console.log(population);*/
+
+//LECTURE: Data Types
+var isIsland = File;
+var language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+//LECTURE: let, const and var
+var language = "Dari";
+const country = "Afghanistan"; 
+const continent = 'Asia';
+language = "Pashto";
